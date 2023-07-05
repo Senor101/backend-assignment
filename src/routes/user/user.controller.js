9a916fbaade06cac666f52d0611336dfc8894d0c
Senor101 <adikshyanta456@gmail.com>
2023-07-05 15:11:51 +0545

    new branch route/user init

--- src/routes/user/user.controller.js
+++ src/routes/user/user.controller.js
@@ -4,8 +4,11 @@ const register = async (req, res, next) => {};
 
 const login = async (req, res, next) => {};
 
+const logout = async (req, res, next) => {};
+
 module.exports = {
   getUsers,
   register,
   login,
+  logout,
 };
